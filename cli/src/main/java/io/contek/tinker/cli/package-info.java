ccf408b8536e502011fc019cc67fb90500dc4425
bjy <bjy@contek.io>
2022-12-12 10:55:00 +0800

    no more cli

--- cli/src/main/java/io/contek/tinker/cli/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@ReturnValuesAreNonnullByDefault
-package io.contek.tinker.cli;
-
-import edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
